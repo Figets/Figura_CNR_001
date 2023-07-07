5614860808958d29695986ed49260d1f53ae14ff
Armanndo Brito <armando.brito@udla.edu.ec>
2023-07-07 08:09:24 -0500

    Figura con Regristro

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-import java.sql.SQLOutput;
+
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -32,6 +32,7 @@ public class Main {
                         System.out.print("El Punto " + (i + 1) + " del Triangulo (x , y): ");
                         double x = scanner.nextDouble();
                         double y = scanner.nextDouble();
+
                         scanner.nextLine();
                         Punto punto = new Punto(x, y);
                         puntosTriangulo.add(punto);
@@ -46,10 +47,12 @@ public class Main {
                     System.out.print("El Punto 1 del Cuadrado (x , y): ");
                     double x1 = scanner.nextDouble();
                     double y1 = scanner.nextDouble();
+
                     scanner.nextLine();
                     System.out.print("El Punto 2 del Cuadrado (x , y): ");
                     double x2 = scanner.nextDouble();
                     double y2 = scanner.nextDouble();
+
                     scanner.nextLine();
                     Punto punto1 = new Punto(x1, y1);
                     Punto punto2 = new Punto(x2, y2);
@@ -64,6 +67,7 @@ public class Main {
                     System.out.print("El Centro del Círculo (x , y): ");
                     double x = scanner.nextDouble();
                     double y = scanner.nextDouble();
+
                     scanner.nextLine();
                     System.out.print("El radio del Círculo: ");
                     double radio = scanner.nextDouble();
@@ -81,12 +85,13 @@ public class Main {
                     List<Punto> puntosPerimetro = new ArrayList<>();
 
                     for (int i = 1; i <= 3; i++) {
-                        System.out.print("El Punto " + i + " de la Piramide (x , y): ");
+                        System.out.print("El Punto " + i + " de la Piramide (x , y , z): ");
                         double a = scanner.nextDouble();
                         double b = scanner.nextDouble();
+                        double z = scanner.nextDouble();
                         scanner.nextLine();
 
-                        Punto punto = new Punto(a, b);
+                        Punto punto = new Punto(a, b, z);
                         puntosPerimetro.add(punto);
                     }
 
@@ -103,16 +108,18 @@ public class Main {
                     System.out.println();
                     break;
                 case "5":
-                    System.out.print("Punto 1 del Cubo (x , y): ");
+                    System.out.print("Punto 1 del Cubo (x , y , z): ");
                     double c = scanner.nextDouble();
                     double d = scanner.nextDouble();
+                    double c1 = scanner.nextDouble();
                     scanner.nextLine();
-                    Punto punto01 = new Punto(c, d);
-                    System.out.print("Punto 2 del Cubo (x , y): ");
+                    Punto punto01 = new Punto(c, d, c1);
+                    System.out.print("Punto 2 del Cubo (x , y , z): ");
                     double e = scanner.nextDouble();
                     double f = scanner.nextDouble();
+                    double e1 = scanner.nextDouble();
                     scanner.nextLine();
-                    Punto punto02 = new Punto(e, f);
+                    Punto punto02 = new Punto(e, f, e1);
                     Cubo cubo = new Cubo(punto01, punto02);
                     registroFiguras.add(cubo);
                     System.out.println("Area del Cubo: " + cubo.calcularArea());
@@ -121,11 +128,12 @@ public class Main {
                     System.out.println();
                     break;
                 case "6":
-                    System.out.print("Centro de la Esfera (x , y): ");
+                    System.out.print("Centro de la Esfera (x , y, z): ");
                     double g = scanner.nextDouble();
                     double h = scanner.nextDouble();
+                    double g1 = scanner.nextDouble();
                     scanner.nextLine();
-                    Punto centro01 = new Punto(g, h);
+                    Punto centro01 = new Punto(g, h, g1);
 
                     System.out.print("El valor del radio de la Esfera: ");
                     double radio01 = scanner.nextDouble();
--- src/Punto.java
+++ src/Punto.java
@@ -6,15 +6,24 @@ public class Punto {
     public Punto(double x, double y) {
         this.x = x;
         this.y = y;
+        this.z = 0;
     }
 
+    public Punto(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
 
     public double calcularDistancia(Punto otroPunto){
         double distanciaX=this.x - otroPunto.x;
         double distanciaY=this.y - otroPunto.y;
-        double distancia = Math.sqrt(distanciaX * distanciaX + distanciaY * distanciaY);
+        double distanciaZ=this.z - otroPunto.z;
+        double distancia = Math.sqrt(distanciaX * distanciaX + distanciaY * distanciaY + distanciaZ * distanciaZ);
         return distancia;
     }
 
-    
+    public double getZ() {
+        return z;
+    }
 }
